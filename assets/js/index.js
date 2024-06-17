76be0a13435e095e617541baf18e2150b441ddb0
Koroliuk8danil <166147781+Koroliuk8danil@users.noreply.github.com>
2024-06-17 22:50:32 +0200

    upload files

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+// function validateAge(value) {
+//   if (typeof value !== "number") {
+//     throw new Error("age must be number");
+//   }
+//   if (value < 18 || value > 125) {
+//     throw new Error("user must be adult");
+//   }
+//   return value;
+// }
+
+// try {
+//   const age = 250;
+//   const validateAge = validateAge(age);
+//   console.log(validateAge);
+// } catch (error) {
+//   alert("Виникла помилка :>> + error.message");
+//   console.dir(error);
+// }
+// console.log("code after ERROR ");
---
+++ assets/js/loops.js
@@ -0,0 +1,14 @@
+// let currentPage = 25;
+// const END_PAGE = 0;
+
+// do {
+//   console.log(`Current Page ${currentPage}`);
+//   currentPage--;
+// } while (END_PAGE <= currentPage);
+
+let startPage = 25;
+const END_PAGE = 1;
+
+while (END_PAGE <= startPage) {
+  console.log(startPage++);
+}
